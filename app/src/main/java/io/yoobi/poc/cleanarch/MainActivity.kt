ef2ed8fb0541f964dd0a247604877ecce741a4c1
Yoobi <>
2023-05-21 14:57:45 +0200

    init project

---
+++ app/src/main/java/io/yoobi/poc/cleanarch/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.yoobi.poc.cleanarch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.yoobi.poc.cleanarch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
