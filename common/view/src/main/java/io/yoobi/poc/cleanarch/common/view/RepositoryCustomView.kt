3684084f3827bccac2b051c0420512e328b85bcd
Yoobi <>
2023-06-12 16:06:08 +0200

    dev(common:view): ItemRepositoryCustomView

---
+++ common/view/src/main/java/io/yoobi/poc/cleanarch/common/view/RepositoryCustomView.kt
@@ -0,0 +1,35 @@
+package io.yoobi.poc.cleanarch.common.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.bumptech.glide.Glide
+import io.yoobi.poc.cleanarch.core.view.R
+import io.yoobi.poc.cleanarch.core.view.databinding.ItemRepositoryBinding
+
+class RepositoryCustomView: LinearLayout {
+
+    private val binding: ItemRepositoryBinding
+
+    constructor(context: Context): super(context) {
+        binding = inflateView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs) {
+        binding = inflateView()
+    }
+
+    private fun inflateView(): ItemRepositoryBinding =
+        ItemRepositoryBinding.inflate(LayoutInflater.from(context)).also { addView(it.root) }
+
+    fun init(name: String, author: String, stars: Int, authorAvatar: String) {
+        binding.repositoryName.text = name
+        binding.repositoryAuthor.text = author
+        binding.repositoryStars.text = context.getString(R.string.item_repository_stars, stars)
+        Glide.with(binding.repositoryImage)
+            .load(authorAvatar)
+            .into(binding.repositoryImage)
+    }
+
+}
