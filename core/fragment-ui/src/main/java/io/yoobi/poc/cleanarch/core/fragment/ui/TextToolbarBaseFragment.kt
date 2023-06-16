99d1efb59275b2b7544d108b2c60d451554f6294
Yoobi <>
2023-06-16 16:41:55 +0200

    fix(core:fragment-ui): Using Kapt to fix issue with TextToolbar

--- core/fragment-ui/src/main/java/io/yoobi/poc/cleanarch/core/fragment/ui/TextToolbarBaseFragment.kt
+++ core/fragment-ui/src/main/java/io/yoobi/poc/cleanarch/core/fragment/ui/TextToolbarBaseFragment.kt
@@ -11,8 +11,6 @@ import com.google.android.material.appbar.MaterialToolbar
 import io.yoobi.poc.cleanarch.core.fragment.ui.databinding.IncludeTextToolbarBinding
 import javax.inject.Inject
 
-//TODO: Fix issue !!!! Can't be used in more than one Fragment
-//TODO Probably because of @Inject
 abstract class TextToolbarBaseFragment<T: ViewBinding>(
     bindingInflater: (layoutInflater: LayoutInflater) -> T
 ): BaseFragment<T>(bindingInflater) {
